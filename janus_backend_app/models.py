42eb2398089f329dad5b0a29f7ec0de9e3642e09
tunahansalih <tusame1994>
2018-12-02 04:15:41 +0300

    location model deleted as unnecessary

--- janus_backend_app/models.py
+++ janus_backend_app/models.py
@@ -5,17 +5,9 @@ from django.db import models
 class Vehicle(models.Model):
     vehicle_id = models.CharField(max_length=500, unique=True)
     vehicle_name = models.CharField(max_length=500)
-    vehicle_access_token = models.CharField(max_length=500)
-    vehicle_refresh_token = models.CharField(max_length=500)
     vehicle_location_latitude = models.CharField(max_length=500)
     vehicle_location_longitude = models.CharField(max_length=500)
     vehicle_destination_latitude = models.CharField(max_length=500)
     vehicle_destination_longitude = models.CharField(max_length=500)
 
-class Location(models.Model):
-    lat = models.FloatField()
-    lon = models.FloatField()
-    name = models.CharField(max_length=500)
-    description = models.CharField(max_length=500)
-    location_type = models.CharField(max_length=500)
 
--- janus_backend_app/serializers.py
+++ janus_backend_app/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
-from janus_backend_app.models import Vehicle, Location
+from janus_backend_app.models import Vehicle
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -18,10 +18,4 @@ class VehicleSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Vehicle
-        fields = ('vehicle_id', 'vehicle_name', 'vehicle_access_token', 'vehicle_refresh_token', 'vehicle_location_latitude', 'vehicle_location_longitude', 'vehicle_destination_latitude', 'vehicle_destination_longitude')
-
-class LocationSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Location
-        fields = ('lat', 'lon', 'name', 'description', 'location_type')
+        fields = ('vehicle_id', 'vehicle_name', 'vehicle_location_latitude', 'vehicle_location_longitude', 'vehicle_destination_latitude', 'vehicle_destination_longitude')
